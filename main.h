dedbf6fb37a8399e480a1d38dec2aa23825928b3
kaboyokelvin <kaboyokelvin@gmail.com>
2022-04-18 12:02:36 -0700

    printf

--- main.h
+++ main.h
@@ -1,109 +1,77 @@
-#ifndef HEADER
-#define HEADER
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
 #include <stdarg.h>
-/*macro*/
-#define BUF_LENGTH 1024
 
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+    int plus;
+    int space;
+    int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+    char c;
+    int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* print_nums */
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
+/* print_bases */
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
 int _printf(const char *format, ...);
-void error_format(const char *format);
-int no_conversion(char);
 
-/*in buffer_functions1.c*/
-char *_flush(char *buffer);
-char *fill_buffer(char *buffer, const char *s, int count_c, int s_length);
-void print_buffer(char *buffer, int length);
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
 
+/* get_flag */
+int get_flag(char s, flags_t *f);
 
-/*in formatting_formatters.c*/
-void fill_format(const char *format);
-char *grab_format(const char *s);
+/* print_alpha */
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
 
-/*in conversion_checks1.c*/
-int conversion_char(char *s);
-int conversion_string(char *s);
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
 
-/* get_validity_func.c */
-int (*get_validity_func(char c))(char *s);
+/* print_custom */
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
 
-/*in conversion_checks2.c*/
-int conversion_di(char *s);
+/* print_address */
+int print_address(va_list l, flags_t *f);
 
-/*in unihelper.c*/
-int _is_digit(char c);
-char *_strncpy(char *dest, const char *src, int n);
-int _strlen(const char *);
-int _isdflag(char *c);
-
-/* in unihelper2.c */
-int give_precision(char *, char);
-int give_width(char *, char);
-char *get_result(char *, char *, int p, int w, int slen, int flen, int mlen);
-
-/*in get_mstring_func.c */
-char *(*get_mstring_func(char))(char *, va_list vl);
-
-/* make_string1.c */
-char *make_char(char *s, va_list vl);
-char *make_string(char *s, va_list vl);
-
-
-/* decimal.c */
-char *num_string(int n);
-char *make_decimal(char *s, va_list vl);
-
-/*unsigned.c */
-int conversion_u(char *s);
-char *make_unsigned(char *s, va_list vl);
-char *_utoa(unsigned int);
-
-/*octal.c */
-int conversion_o(char *s);
-char *make_octal(char *s, va_list vl);
-char *octorbi(unsigned int, int);
-
-/*binary.c*/
-int conversion_b(char *s);
-char *make_binary(char *s, va_list vl);
-
-
-/* hex. */
-int conversion_h(char *s);
-char *make_hex(char *s, va_list vl);
-char *hex(unsigned int);
-void reverse_array(char *a, int n);
-
-/* heX */
-char *make_heX(char *s, va_list vl);
-char *heX(unsigned int);
-
-/*structs*/
-/**
- * struct valid - check if formatting is valid
- * @type: type
- * @conversion_check:function to check if a conversion string is valid
- */
-typedef struct valid
-{
-	char type;
-	int (*conversion_check)(char *);
-} valid;
-
-/**
- * struct m_string -  valid format
- * @type: type of format
- * @make_s:make the string
- */
-typedef struct m_string
-{
-	char type;
-	char *(*make_s)(char *, va_list);
-} m_string;
-
-
-
-
-
-
+/* print_percent */
+int print_percent(va_list l, flags_t *f);
 
 
 #endif
