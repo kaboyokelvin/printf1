d1cce0ec21829e22df23f3779ecf1a8352a25795
AyfedTecnolpgies <ayikobuaferdinant1@gmail.com>
2022-04-19 11:43:23 -0700

    printf

--- base_converters.c
+++ base_converters.c
@@ -1,103 +1,101 @@
 #include "main.h"
 
-unsigned int convert_sbase(buffer_t *output, long int num, char *base,
-		unsigned char flags, int wid, int prec);
-unsigned int convert_ubase(buffer_t *output,
-		unsigned long int num, char *base,
-		unsigned char flags, int wid, int prec);
+
 
 /**
- * convert_sbase - Converts a signed long to an inputted base and stores
- *                 the result to a buffer contained in a struct.
- * @output: A buffer_t struct containing a character array.
- * @num: A signed long to be converted.
- * @base: A pointer to a string containing the base to convert to.
- * @flags: Flag modifiers.
- * @wid: A width modifier.
- * @prec: A precision modifier.
  *
- * Return: The number of bytes stored to the buffer.
- */
-unsigned int convert_sbase(buffer_t *output, long int num, char *base,
-		unsigned char flags, int wid, int prec)
-{
-	int size;
-	char digit, pad = '0';
-	unsigned int ret = 1;
-
-	for (size = 0; *(base + size);)
-		size++;
-
-	if (num >= size || num <= -size)
-		ret += convert_sbase(output, num / size, base,
-				flags, wid - 1, prec - 1);
-
-	else
-	{
-		for (; prec > 1; prec--, wid--) /* Handle precision */
-			ret += _memcpy(output, &pad, 1);
-
-		if (NEG_FLAG == 0) /* Handle width */
-		{
-			pad = (ZERO_FLAG == 1) ? '0' : ' ';
-			for (; wid > 1; wid--)
-				ret += _memcpy(output, &pad, 1);
-		}
-	}
-
-	digit = base[(num < 0 ? -1 : 1) * (num % size)];
-	_memcpy(output, &digit, 1);
-
-	return (ret);
-}
-
-/**
- * convert_ubase - Converts an unsigned long to an inputted base and
- *                 stores the result to a buffer contained in a struct.
- * @output: A buffer_t struct containing a character array.
- * @num: An unsigned long to be converted.
- * @base: A pointer to a string containing the base to convert to.
- * @flags: Flag modifiers.
- * @wid: A width modifier.
- * @prec: A precision modifier.
+ *  * _printf - formatted output conversion and print data.
  *
- * Return: The number of bytes stored to the buffer.
- */
-unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
-		unsigned char flags, int wid, int prec)
+ *   * @format: input string.
+ *
+ *    *
+ *
+ *     * Return: number of chars printed.
+ *
+ *      */
+
+int _printf(const char *format, ...)
+
 {
-	unsigned int size, ret = 1;
-	char digit, pad = '0', *lead = "0x";
 
-	for (size = 0; *(base + size);)
-		size++;
+		unsigned int i = 0, len = 0, ibuf = 0;
 
-	if (num >= size)
-		ret += convert_ubase(output, num / size, base,
-				flags, wid - 1, prec - 1);
+			va_list arguments;
 
-	else
-	{
-		if (((flags >> 5) & 1) == 1) /* Printing a ptr address */
-		{
-			wid -= 2;
-			prec -= 2;
-		}
-		for (; prec > 1; prec--, wid--) /* Handle precision */
-			ret += _memcpy(output, &pad, 1);
+				int (*function)(va_list, char *, unsigned int);
 
-		if (NEG_FLAG == 0) /* Handle width */
-		{
-			pad = (ZERO_FLAG == 1) ? '0' : ' ';
-			for (; wid > 1; wid--)
-				ret += _memcpy(output, &pad, 1);
-		}
-		if (((flags >> 5) & 1) == 1) /* Print 0x for ptr address */
-			ret += _memcpy(output, lead, 2);
-	}
+					char *buffer;
 
-	digit = base[(num % size)];
-	_memcpy(output, &digit, 1);
 
-	return (ret);
+
+						va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+
+							if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+
+										return (-1);
+
+								if (!format[i])
+
+											return (0);
+
+									for (i = 0; format && format[i]; i++)
+
+											{
+
+														if (format[i] == '%')
+
+																	{
+
+																					if (format[i + 1] == '\0')
+
+																									{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+
+																														return (-1);
+
+																																	}
+
+																								else
+
+																												{	function = get_print_func(format, i + 1);
+
+																																	if (function == NULL)
+
+																																						{
+
+																																												if (format[i + 1] == ' ' && !format[i + 2])
+
+																																																			return (-1);
+
+																																																	handl_buf(buffer, format[i], ibuf), len++, i--;
+
+																																																					}
+
+																																					else
+
+																																										{
+
+																																																len += function(arguments, buffer, ibuf);
+
+																																																					i += ev_print_func(format, i + 1);
+
+																																																									}
+
+																																								} i++;
+
+																										}
+
+																else
+
+																				handl_buf(buffer, format[i], ibuf), len++;
+
+																		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+
+																						;
+
+																			}
+
+										print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+
+											return (len);
+
 }
